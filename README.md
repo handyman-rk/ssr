67347347b6981b2ce03a564cafa450145d53e479
Rohit kr <rkyiipa@gmail.com>
2018-03-25 18:22:50 +0530

    link to medium story

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # React Server-Side Rendering Example
 
-Above is an example playground for you to play with React on Client and Server sides.
-Before jumping into it, make sure to read SSR tutorial on Medium.
+Above is an example playground for you to play with React & Redux on Client and Server sides.
+Before jumping into it, make sure to [read SSR tutorial on Medium](http://bit.ly/2IQ51Xq).
+
+
+## Ready to become a React Pro?
+Subscribe to get your React skills blazing, immediately. 
+
+[Join the mailing list](http://eepurl.com/dpfhGn).  I won’t spam. 👍
--- views/redux/reducers.js
+++ views/redux/reducers.js
@@ -1,21 +1,19 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-
-var _actions = require('./actions');
 
 function apps() {
   var state = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : { isFetching: false, apps: [] };
   var action = arguments[1];
 
   switch (action.type) {
-    case _actions.REQUEST_APPS:
+    case REQUEST_APPS:
       return Object.assign({}, state, {
         isFetching: true
       });
-    case _actions.RECEIVE_APPS:
+    case RECEIVE_APPS:
       return Object.assign({}, state, {
         isFetching: false,
         apps: action.apps
